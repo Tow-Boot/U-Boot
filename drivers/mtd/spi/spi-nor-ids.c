38bdc8d57bbcca03708b0e450d4daed0b586bd62
Ulrik Strid <ulrik.strid@outlook.com>
2022-04-26 09:27:57 +0200

    Add support for XTX SPI XT25Q64D
    Signed-off-by: Ulrik Strid <ulrik.strid@outlook.com>

--- drivers/mtd/spi/spi-nor-ids.c
+++ drivers/mtd/spi/spi-nor-ids.c
@@ -531,6 +531,7 @@ const struct flash_info spi_nor_ids[] = {
 	/* XTX Technology (Shenzhen) Limited */
 	{ INFO("xt25f128b", 0x0b4018, 0, 64 * 1024, 256, SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
 	{ INFO("xt25f32b",  0x0b4016, 0, 64 * 1024,  64, SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
+	{ INFO("xt25q64d",  0x0b6017, 0, 64 * 1024, 128, SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
 #endif
 	{ },
 };
