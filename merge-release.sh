91d011ee3d86a9a15f858798f7a1f27e73f344e9
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-06-23 20:23:10 -0400

    merge-release: Add contributors list

--- merge-release.sh
+++ merge-release.sh
@@ -11,11 +11,20 @@ fi
 RELEASE="$1"
 shift
 PREFIX="tow-boot/$RELEASE"
+UBOOT_TAG="v${RELEASE}"
 
 branches() {
 	git branch --all --format '%(refname:lstrip=2)' --list "$PREFIX"/* \
 		| grep -v "$PREFIX/_all" \
 		| sort
+}
+
+branch_details() {
+	for branch in $(branches); do
+		printf " - %s ⇒ %s\n" \
+			"$(git rev-parse "$branch")" \
+			"$branch"
+	done
 }
 
 message() {
@@ -24,11 +33,20 @@ Integration branch for Tow-Boot ${RELEASE}
 
 This branch includes changes from the following branches:
 
-$(branches | sed -e 's/^/ - /')
+$(branch_details)
+
+Including contributions from:
+$(
+	git shortlog --summary --numbered --no-merges \
+		--group=author --group=trailer:co-authored-by --group=trailer:reviewed-by \
+		"${UBOOT_TAG}..${PREFIX}/_all" \
+		| sed -e 's/^/ - /'
+)
 
 EOF
 }
 
-git checkout -B "$PREFIX/_all" "v${RELEASE}"
+git checkout -B "$PREFIX/_all" "$UBOOT_TAG"
 #shellcheck disable=SC2046
-git merge --no-ff $(branches) -m "$(message)"
+git merge --no-ff $(branches) -m "[TBD]"
+git commit --amend -m "$(message)"
