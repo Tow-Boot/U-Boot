5fb9ab07e0d0c0ecba070df0ce04a40814265da8
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-06-28 17:44:17 -0400

    Add remote-update helper

---
+++ remote-update.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+PS4=" $ "
+
+this="${BASH_SOURCE[0]%/*}"
+
+if (( $# < 2 )); then
+	echo "Usage: $0 <remote name> <20yy.mm>"
+	exit 1
+fi
+
+REMOTE="$1"; shift
+TAG="$1"; shift
+
+set -x
+
+# shellcheck disable=2046
+git push --force -u "$REMOTE" $("$this"/ls.sh "$TAG") "tow-boot/$TAG/_all"
