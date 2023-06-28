1260a9e55ae5e3944392daa8940f88f330ca82b1
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-06-28 15:38:14 -0400

    Add remote-cleanup helper

---
+++ remote-cleanup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+PS4=" $ "
+
+if (( $# < 2 )); then
+	echo "Usage: $0 <remote name> <branch prefix>"
+	exit 1
+fi
+
+REMOTE="$1"; shift
+PREFIX="$1"; shift
+
+set -x
+git fetch -p "${REMOTE}"
+git branch -r \
+	| grep "${REMOTE}"/"${PREFIX}"/ \
+	| grep -v '_all' \
+	| cut -d'/' -f2- \
+	| xargs -I {} git push tow-boot :{}
